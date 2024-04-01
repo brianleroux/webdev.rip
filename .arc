a772430fbdc73c3b3b205620493751e30190a224
brianleroux <b@brian.io>
2024-04-01 15:03:45 -0700

    closes #7

--- .arc
+++ .arc
@@ -3,6 +3,7 @@ begin-app
 
 @plugins
 enhance/arc-plugin-enhance
+enhance/arc-image-plugin
 
 @begin
 appID K7833DPN
--- app/preflight.mjs
+++ app/preflight.mjs
@@ -9,13 +9,22 @@ export default function preflight () {
         title: 'First post!', 
         summary: 'The hello world post.', 
         link: '/notes/first-post', 
+        tags: ['webdev', 'serverless', 'philosophy'],
         date: '2024-03-28' 
       },
       {  
         title: 'The Problem with Solutions', 
         summary: 'The best solution to a problem is eliminating the conditions that create that problem rather than adding another solution that creates new unique problems.',
         link: '/notes/the-problem-with-solutions', 
+        tags: ['webdev', 'serverless', 'philosophy'],
         date: '2024-03-29' 
+      },
+      {  
+        title: 'Retro Gaming', 
+        summary: 'Testing out responsive images with custom elements and Metroid Fusion.',
+        link: '/notes/retro-gaming', 
+        tags: ['retrogaming'],
+        date: '2024-04-01' 
       },
     ]
   }
