628022c1bb0c2cfbc4c9c6d9c3024c96aafd7771
brianleroux <b@brian.io>
2024-04-03 13:37:20 -0700

    add a little cache

--- og-img/index.mjs
+++ og-img/index.mjs
@@ -2,6 +2,8 @@ import preflight from '@architect/views/preflight.mjs'
 import arc from '@architect/functions'
 import render from './render.mjs'
 
+const cache = {} // cheesy coldstart cache
+
 export let handler = arc.http(fn)
 
 async function fn (req) {
@@ -15,21 +17,29 @@ async function fn (req) {
     html: `Could not find: ${ title }` 
   }
 
-  let out = await render({ 
-    title: meta.title, 
-    link: `https://webdev.rip${ meta.link }`, 
-    summary: meta.summary, 
-    author: 'Brian LeRoux',
-    published:  'Posted on ' + new Intl.DateTimeFormat('en-US', {dateStyle: 'long'}).format(new Date(meta.date)), 
-  })
+  if (!cache[title]) {
+    console.log('rendering title')
+    cache[title] = (await render({ 
+      title: meta.title, 
+      link: `https://webdev.rip${ meta.link }`, 
+      summary: meta.summary, 
+      author: 'Brian LeRoux',
+      published:  'Posted on ' + new Intl.DateTimeFormat('en-US', {dateStyle: 'long'}).format(new Date(meta.date)), 
+    })).toString('base64')
+  }
+
+  // set cache-control headers in production
+  let cacheControl = process.env.ARC_ENV === 'production'
+    ? 'max-age=3600;'
+    : 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
 
   return {
     statusCode: 200,
     headers: { 
       'content-type': 'image/jpeg', 
-      'cache-control': 'max-age=1800' // 30m cache 
+      'cache-control': cacheControl
     },
     isBase64Encoded: true,
-    body: out.toString('base64')
+    body: cache[title]
   } 
 }
